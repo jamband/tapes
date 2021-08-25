9190aa3878a33c45edee8af0df83d4750939a22c
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-26 02:01:47 +0900

    Improve footer styles

--- src/layouts/footer/component.svelte
+++ src/layouts/footer/component.svelte
@@ -5,27 +5,28 @@
   import { track } from "$stores/track";
 </script>
 
-<footer class="fixed bottom-0 w-full text-sm bg-gray-800">
+<footer class="fixed bottom-0 w-full bg-gray-800">
   {#if $track.path && !$page.params.track}
-    <nav class="flex items-center justify-center font-semibold">
-      <IconVolumeUp class="mx-3 h-4 w-4" />
+    <div class="py-1 md:container md:mx-auto flex items-center justify-center">
+      <span class="ml-4"><IconVolumeUp class="h-4 w-4" /></span>
       <a
-        class="whitespace-nowrap overflow-ellipsis overflow-hidden hover:text-purple-500"
+        class="ml-4 mr-1 whitespace-nowrap overflow-ellipsis overflow-hidden font-semibold text-sm hover:text-purple-500"
         href={$track.path}>{$track.title}</a
       >
-      <button class="p-3" on:click={track.clear}
+      <button class="mr-2 p-2" on:click={track.clear}
         ><IconX class="h-5 w-5 hover:text-purple-500" /></button
       >
-    </nav>
+    </div>
   {:else}
-    <nav class="container mx-auto text-gray-300" aria-label="Footer navigation">
-      <div
-        class="py-3 grid grid-cols-3 divide-x divide-gray-600 font-mono font-light text-xs"
-      >
-        <a class="text-center hover:text-purple-500" href="/about">About</a>
-        <a class="text-center hover:text-purple-500" href="/contact">Contact</a>
+    <nav
+      class="py-3 container mx-auto font-mono font-light text-xs text-gray-300"
+      aria-label="Footer navigation"
+    >
+      <div class="grid grid-cols-3 divide-x divide-gray-600 text-center">
+        <a class="hover:text-purple-500" href="/about">About</a>
+        <a class="hover:text-purple-500" href="/contact">Contact</a>
         <LinkExternal
-          class="text-center hover:text-purple-500"
+          class="hover:text-purple-500"
           href="https://github.com/jamband/tapes">GitHub</LinkExternal
         >
       </div>
