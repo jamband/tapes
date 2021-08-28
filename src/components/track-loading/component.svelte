2d7a8c0bd339faadfa11e46f98c4dc6c8fa29544
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-28 12:51:02 +0900

    Change track loading styles

--- src/components/track-loading/component.svelte
+++ src/components/track-loading/component.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <div class={$$props.class || ""} role="status">
-  <IconLightningBolt class="mr-2 h-3 w-3 animate-ping" />
+  <IconLightningBolt class="mb-2 h-3 w-3 animate-ping" />
   <span class="font-mono text-xs text-gray-400">Loading...</span>
 </div>
