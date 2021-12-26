ad18d6ff8c65dc11cb771562a3f97b441f755e5e
Tomoki Morita <tmsongbooks215@gmail.com>
2021-12-27 02:38:51 +0900

    Improve player loading style

--- src/components/track/component.svelte
+++ src/components/track/component.svelte
@@ -1,16 +1,41 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import { beforeUpdate } from "svelte";
   import { SectionDivider } from "~/components/section-divider";
+  import { IconLoading } from "~/icons";
   import { player, track, trackId } from "~/stores/track";
 
   $: isSquareRatio = ["Bandcamp", "SoundCloud"].includes($track.provider);
   $: tapePath = $track.path.split("/").slice(0, -1).join("/");
+
+  let title = "";
+  let playerLoading = true;
+
+  const playerLoaded = () => {
+    playerLoading = false;
+    title = $track.title;
+  };
+
+  beforeUpdate(() => {
+    if (title !== $track.title) {
+      playerLoading = true;
+    }
+  });
 </script>
 
 {#if $trackId}
   <div class="mb-3 grid grid-cols-6 grap-4">
-    <div class="col-span-6 {isSquareRatio && 'md:col-span-4 md:col-start-2'}">
+    <div
+      class="relative col-span-6 {isSquareRatio &&
+        'md:col-span-4 md:col-start-2'}"
+    >
       {#key $trackId}
+        <div
+          class="pointer-events-none absolute inset-0 flex items-center justify-center"
+          class:hidden={!playerLoading}
+        >
+          <IconLoading class="h-6 w-6 text-violet-500" />
+        </div>
         <iframe
           src={$player}
           class="w-full rounded"
@@ -18,6 +43,7 @@
           class:aspect-video={!isSquareRatio}
           title="{$track.title} ･ {$track.provider}"
           allowfullscreen
+          on:load={() => playerLoaded()}
         />
       {/key}
     </div>
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as IconExternalLink } from "./_external-link.svelte";
+export { default as IconLoading } from "./_loading.svelte";
 export { default as IconPause } from "./_pause.svelte";
 export { default as IconPlay } from "./_play.svelte";
 export { default as IconVolumeUp } from "./_volume-up.svelte";
