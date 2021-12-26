0d1b5b3918683c4ebde54f7860245e35a29cadcc
Tomoki Morita <tmsongbooks215@gmail.com>
2021-12-26 23:48:50 +0900

    Minimal rounded on section-divider component

--- src/components/section-divider/component.svelte
+++ src/components/section-divider/component.svelte
@@ -5,11 +5,11 @@
   class="{$$props.class || ''} flex items-center justify-center"
   role="separator"
 >
-  <span class="mr-1 bg-violet-600 h-1 w-1" />
-  <span class="mr-1 bg-violet-700 h-3 w-1" />
-  <span class="mr-1 bg-violet-900 h-6 w-1" />
-  <span class="mr-1 bg-violet-800 h-4 w-1" />
-  <span class="mr-1 bg-violet-600 h-7 w-1" />
-  <span class="mr-1 bg-violet-700 h-3 w-1" />
-  <span class="bg-violet-500 h-1 w-1" />
+  <span class="mr-1 bg-violet-600 h-1 w-1 rounded-[1px]" />
+  <span class="mr-1 bg-violet-700 h-3 w-1 rounded-[1px]" />
+  <span class="mr-1 bg-violet-900 h-6 w-1 rounded-[1px]" />
+  <span class="mr-1 bg-violet-800 h-4 w-1 rounded-[1px]" />
+  <span class="mr-1 bg-violet-600 h-7 w-1 rounded-[1px]" />
+  <span class="mr-1 bg-violet-700 h-3 w-1 rounded-[1px]" />
+  <span class="bg-violet-500 h-1 w-1 rounded-[1px]" />
 </div>
