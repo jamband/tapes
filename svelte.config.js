aa423a52ed1b2ca8e8f9011e7141f32e9b9d7dd7
Tomoki Morita <tmsongbooks215@gmail.com>
2022-09-01 20:33:28 +0900

    Fix some options related to prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,6 @@ const config = {
     paths: {
       base: process.env["VITE_GITHUB_ACTIONS"] ? "/tapes" : "",
     },
-    prerender: {
-      default: true,
-    },
     trailingSlash: "always",
   },
 };
