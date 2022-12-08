a9cd0c4ec519b51dc9b8c4f4b47d868badefbd51
Tomoki Morita <tmsongbooks215@gmail.com>
2022-12-08 22:27:08 +0900

    use gap instead of margin in section divider component

--- src/components/section-divider/component.svelte
+++ src/components/section-divider/component.svelte
@@ -3,12 +3,15 @@
   export { className as class };
 </script>
 
-<div class="flex items-center justify-center {className}" role="separator">
-  <span class="mr-1 h-1 w-1 rounded-[1px] bg-purple-500" />
-  <span class="mr-1 h-3 w-1 rounded-[1px] bg-purple-600" />
-  <span class="mr-1 h-6 w-1 rounded-[1px] bg-purple-800" />
-  <span class="mr-1 h-4 w-1 rounded-[1px] bg-purple-700" />
-  <span class="mr-1 h-7 w-1 rounded-[1px] bg-purple-500" />
-  <span class="mr-1 h-3 w-1 rounded-[1px] bg-purple-600" />
+<div
+  class="flex gap-x-1 items-center justify-center {className}"
+  role="separator"
+>
+  <span class="h-1 w-1 rounded-[1px] bg-purple-500" />
+  <span class="h-3 w-1 rounded-[1px] bg-purple-600" />
+  <span class="h-6 w-1 rounded-[1px] bg-purple-800" />
+  <span class="h-4 w-1 rounded-[1px] bg-purple-700" />
+  <span class="h-7 w-1 rounded-[1px] bg-purple-500" />
+  <span class="h-3 w-1 rounded-[1px] bg-purple-600" />
   <span class="h-1 w-1 rounded-[1px] bg-purple-400" />
 </div>
