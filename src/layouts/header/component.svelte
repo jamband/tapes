a095ad5009d82926e5f2d09ebd381c86d7606d74
Tomoki Morita <tmsongbooks215@gmail.com>
2022-12-22 21:32:47 +0900

    fix header component styles

--- src/layouts/header/component.svelte
+++ src/layouts/header/component.svelte
@@ -13,13 +13,13 @@
       class="m-1 inline-block rounded px-4 py-1 no-underline hover:bg-gray-600 active:bg-gray-600"
     >
       <span
-        class="text-xs font-semibold text-gray-400 {$page.params.track
+        class="text-xs font-semibold {$page.params.track
           ? 'text-gray-100 duration-1000'
-          : ''}">{name}/</span
+          : 'text-gray-400'}">{name}/</span
       ><span
-        class="font-bold text-gray-100 {$page.params.track
+        class="font-bold {$page.params.track
           ? 'text-purple-400 duration-1000'
-          : ''}">{repository}</span
+          : 'text-gray-100'}">{repository}</span
       >
     </a>
   </nav>
