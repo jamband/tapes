776eb78cbdb354eda66557e2c7240a1761a7db73
Tomoki Morita <tmsongbooks215@gmail.com>
2023-02-04 13:10:26 +0900

    simplify header styles

--- src/layouts/header/component.svelte
+++ src/layouts/header/component.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { base } from "$app/paths";
-  import { page } from "$app/stores";
   import { APP_NAME } from "@/constants/app";
 
   const [name, repository] = APP_NAME.split("/");
@@ -10,17 +9,10 @@
   <nav class="my-2" aria-label="Header navigation">
     <a
       href="{base}/"
-      class="m-1 inline-block rounded px-4 py-1 no-underline hover:bg-gray-600 active:bg-gray-600"
+      class="m-1 inline-block rounded px-4 py-1 font-bold no-underline hover:bg-gray-600 active:bg-gray-600"
+      ><span class="text-xs text-gray-100">{name}/</span><span
+        class="text-purple-400">{repository}</span
+      ></a
     >
-      <span
-        class="text-xs font-semibold {$page.params.track
-          ? 'text-gray-100 duration-1000'
-          : 'text-gray-400'}">{name}/</span
-      ><span
-        class="font-bold {$page.params.track
-          ? 'text-purple-400 duration-1000'
-          : 'text-gray-100'}">{repository}</span
-      >
-    </a>
   </nav>
 </header>
