fdfe2ecc88bf70c1886b7d00d7a4b50ee34b2801
Tomoki Morita <tmsongbooks215@gmail.com>
2024-10-20 17:17:27 +0900

    remove unnecessary generic type argument

--- src/icons/_base/types.ts
+++ src/icons/_base/types.ts
@@ -3,7 +3,7 @@ import type { Snippet } from "svelte";
 export type Props = {
   class: string;
   viewBox: `${number} ${number} ${number} ${number}`;
-  children: Snippet<[]>;
+  children: Snippet;
 };
 
 export type IconProps = Pick<Props, "class">;
