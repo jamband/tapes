5c08d27092bc31d64c9d9b31548aaba7c34aa46c
Tomoki Morita <tmsongbooks215@gmail.com>
2025-09-24 18:08:38 +0900

    pin sveltekit version to 2.42.2
    and some fixes
    because some errors occur during build with sveltekit 2.43.x

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,8 @@ const config = {
     adapter: adapter({ fallback: "404.html" }),
     alias: { "@/*": "src/*" },
     paths: {
-      base: "/tapes",
+      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+      relative: false,
     },
   },
 };
